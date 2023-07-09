39c4abb0b1743c1c98f4b283e2a6216e5f956438
Rene Corrales <ruckonic@icloud.com>
2023-07-09 17:08:35 -0600

    remove unused component

--- src/screens/RssListScreen.jsx
+++ src/screens/RssListScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {SafeAreaView, Animated, WebView, StyleSheet, View} from 'react-native'
+import {SafeAreaView, Animated, StyleSheet, View} from 'react-native'
 
 import Header from '../components/Header'
 import {RssList} from '../components/rss/RssList'
